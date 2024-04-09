5b0616e10bc0f69abb7db2f7e8d198f23f255b1c
z-e-a <e.a.zubkov@yandex.ru>
2024-04-09 17:53:35 +0300

    feat: solve task 'getFirstPromiseResult'

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -79,8 +79,12 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    Promise.any(promises)
+      .then((value) => resolve(value))
+      .catch((message) => reject(message));
+  });
 }
 
 /**
