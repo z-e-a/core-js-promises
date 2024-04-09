275e6ef1a9e8cabe699e77fbc0c92c801fb8bf43
z-e-a <e.a.zubkov@yandex.ru>
2024-04-09 13:38:43 +0300

    feat: solve task 'getPromise'

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,8 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return number < 0 ? Promise.reject(number) : Promise.resolve(number);
 }
 
 /**
