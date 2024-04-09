12968ce23914f506d311f1fbda5d801d5da73559
z-e-a <e.a.zubkov@yandex.ru>
2024-04-09 17:47:14 +0300

    feat: solve task 'getFirstResolvedPromiseResult'

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -52,8 +52,12 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    Promise.race(promises)
+      .then((value) => resolve(value))
+      .catch((message) => reject(message));
+  });
 }
 
 /**
